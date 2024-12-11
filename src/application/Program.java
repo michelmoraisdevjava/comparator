56c86f0eb66e3127ec0dce6e2adda6281fb94815
Michel Morais <valentimmichel@gmail.com>
2024-12-11 13:48:53 -0300

    Update: Comparator with anonymous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
@@ -17,8 +18,14 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		//Utilizando o Comparator de classe separada
-		Collections.sort(list, new ProductComparator());
+		//Usando Comparator com classe anônima
+		Collections.sort(list, new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		});
 		
 		for(Product p : list) {
 			System.out.println(p);
